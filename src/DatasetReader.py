83c06e4adac39c56cf92856c6380fda92ea3792c
raina4 <raina4@illinois.edu>
2017-11-17 22:42:13 -0600

    fixed gen ratings

--- src/DatasetReader.py
+++ src/DatasetReader.py
@@ -293,16 +293,16 @@ class DatasetReader:
 		num_songs = len(song_db.keys())
 		# a matrix of normalized counts
 		ratings_mat = csc_matrix((num_users, num_songs), dtype=np.int16)
-		max_song_rating = numpy.zeros(1,num_songs)
+		max_song_rating = np.zeros((num_songs))
 		for user_id, user_obj in user_db.items():
 			user_id_int = int(user_id[5:])
 
-			for song_obj,count in user_obj.songs.items():
-				song_id_int = song_db[song_id].song_id_int
+			for song_id, count in user_obj.songs.items():
+				song_id_int = int(song_db[song_id].song_id_int)
 				# normalized count
-				rating = np.log(count/user_obj.num_songs_played + 1)
-				ratings_mat[user_id, song_id_int] = rating
-				max_song_rating = max(max_song_rating[song_id_ing], rating)
+				rating = np.log(float(count)/float(user_obj.num_songs_played) + 1)
+				ratings_mat[user_id_int, song_id_int] = rating
+				max_song_rating[song_id_int] = max(max_song_rating[song_id_int], rating)
 
 		ratings_mat.dot(diags([1/max_song_rating])) * 5
 		np.save('../datasets/lastfm-dataset-1K/extracts/rating_mat', ratings_mat)
