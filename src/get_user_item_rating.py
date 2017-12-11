5200f8076769e4f3ea5e7ff29572d33feb42c827
shalan12 <naqvi5@illinois.edu>
2017-12-10 23:12:21 -0600

    code for ratings and hop distances

--- src/get_user_item_rating.py
+++ src/get_user_item_rating.py
@@ -6,24 +6,42 @@ from scipy.sparse import load_npz, csr_matrix, coo_matrix
 ratings_mat = load_npz('../datasets/lastfm-dataset-1K/extracts/rating_mat.npz')
 test_songs_for_user2 = np.loadtxt('../datasets/lastfm-dataset-1K/extracts/test_songs_user_000002',
          dtype=np.int)
+train_songs_for_user2 = np.loadtxt('../datasets/lastfm-dataset-1K/extracts/train_songs.txt',
+                                   dtype=np.int, delimiter=',')
 user2_ratings = ratings_mat.getrow(1)
 
 user2_ratings = user2_ratings.toarray()[0] # convert to a normal array
 # print(user2_ratings.shape)
 train_ratings = np.copy(user2_ratings)
-train_ratings[test_songs_for_user2] = 0
-test_ratings = user2_ratings[test_songs_for_user2]
+# train_ratings = np.copy(ratings_mat.toa)
+non_train_idxs = np.array(list(set(range(user2_ratings.shape[0])) - set(train_songs_for_user2)))
+train_ratings[non_train_idxs] = 0
+test_ratings = np.copy(user2_ratings)
+non_test_idxs = np.array(list(set(range(user2_ratings.shape[0])) - set(test_songs_for_user2)))
+test_ratings[non_test_idxs] = 0
 
 # output train data
-train_ratings = csr_matrix(train_ratings).tocoo()
-data, row, col = train_ratings.data, train_ratings.row, train_ratings.col
-row += 2
-output  = '\n'.join([' '.join([str(x) for x in entry]) for entry in zip(row,col,data)])
+def get_output_string(ratings, user, skip_user, cols_to_include=None):
+    train_ratings = ratings.tocoo()
+    data, row, col = train_ratings.data, train_ratings.row, train_ratings.col
+    if skip_user:
+        output  = '\n'.join([' '.join([str(x) for x in entry])
+                             for entry in zip(row,col,data)
+                             if entry[0] != user and entry[1] in cols_to_include])
+    else:
+        row += user
+        output  = '\n'.join([' '.join([str(x) for x in entry]) for entry in zip(row,col,data)])
+    return output
+
+output = get_output_string(csr_matrix(train_ratings), 2, False)
+output += '\n'
+user2_songs = set(train_songs_for_user2) | set(test_songs_for_user2)
+output += get_output_string(ratings_mat, 2, True, user2_songs)
 f = open('../datasets/lastfm-dataset-1K/extracts/trainset_user_000002', 'w')
 f.write(output)
 f.close()
 
-# output train data
+# output test data
 test_ratings = csr_matrix(test_ratings).tocoo()
 data, row, col = test_ratings.data, test_ratings.row, test_ratings.col
 row +=2
